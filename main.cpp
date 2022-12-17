f55995167e31b48a3517d194434fc907a713d50d
irenemassa03 <114859857+irenemassa03@users.noreply.github.com>
2022-12-17 11:54:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char a[10][20];
+  char b[1][20];
+  for (int i=0; i<10; i++){
+    for (int j=0; j<20; j++){
+      a[i][j]= ' ';
+    }
+    cin >> a[i];
+  }
+  for (int j=0; j<20;j++){
+    b[0][j]= ' ';
+  }
+  cin >> b[0];
+  if(nomi(a,b)==-1){
+    cout << "non presente";
+  }else{
+    cout << nomi(a,b);
+  }
   return 0;
 }
